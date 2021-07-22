dc04f46f6b08f18472488e0adddeb906d89503de
federzvz <85709865+federzvz@users.noreply.github.com>
2021-07-22 16:26:02 -0300

    Create LoginSystem.cpp

---
+++ LoginSystem.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   LoginSystem.cpp
+ * Author: Admin
+ * 
+ * Created on 22 de julio de 2021, 01:55 PM
+ */
+
+#include "LoginSystem.h"
+
+bool LoginAdmin (string username, string password){
+    
+}
+
+bool LoginDocente (string username, string password){
+    
+}
+
+bool LoginEstudiante (string username, string password){
+    
+}
