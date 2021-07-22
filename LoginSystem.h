8b16d78ec68908e05562dc80b212e0b9892736f1
federzvz <85709865+federzvz@users.noreply.github.com>
2021-07-22 16:26:34 -0300

    Create LoginSystem.h

---
+++ LoginSystem.h
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   LoginSystem.h
+ * Author: Admin
+ *
+ * Created on 22 de julio de 2021, 01:33 PM
+ */
+
+#ifndef LOGINSYSTEM_H
+#define LOGINSYSTEM_H
+#include <string>
+
+using namespace std;
+
+bool LoginAdmin(string,string);
+bool LoginDocente(string,string);
+bool LoginEstudiante(string,string);
+
+
+#endif /* LOGINSYSTEM_H */
