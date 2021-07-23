fddaea36be0d7d8555ad053fe9ec5b39af86adb5
DieterPreuss <87830546+DieterPreuss@users.noreply.github.com>
2021-07-22 21:12:32 -0300

    Update DtTimeStamp.cpp

--- DtTimeStamp.cpp
+++ DtTimeStamp.cpp
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   DtTimeStamp.cpp
- * Author: Admin
- * 
- * Created on 21 de julio de 2021, 08:17 PM
- */
 
 #include "DtTimeStamp.h"
 
-DtTimeStamp::DtTimeStamp() {
+DtTimeStamp::DtTimeStamp(int hr_x,int min_x,int seg_x,DtFecha fecha_x) {
+    this->hr=hr_x;
+    this->min=min_x;
+    this->seg=seg_x;
+    this->fecha=fecha_x;
 }
 
 DtTimeStamp::DtTimeStamp(const DtTimeStamp& orig) {
@@ -22,20 +14,11 @@ DtTimeStamp::DtTimeStamp(const DtTimeStamp& orig) {
 DtTimeStamp::~DtTimeStamp() {
 }
 
-void DtTimeStamp::setHr(int hr_x){
-    this->hr=hr_x;
-}
 int DtTimeStamp::getHr(){
     return this->hr;
 }
-void DtTimeStamp::setMin(int min_x){
-    this->min=min_x;
-}
 int DtTimeStamp::getMin(){
     return this->min;
-}
-void DtTimeStamp::setSeg(int seg_x){
-    this->seg=seg_x;
 }
 int DtTimeStamp::getSeg(){
     return this->seg;
