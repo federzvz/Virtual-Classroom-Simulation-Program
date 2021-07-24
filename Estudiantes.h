366c407505ac80c68427397c183f39ea32eeaa79
federzvz <85709865+federzvz@users.noreply.github.com>
2021-07-24 20:05:06 -0300

    Update Estudiantes.h

--- Estudiantes.h
+++ Estudiantes.h
@@ -4,19 +4,23 @@
 using namespace std;
 #include "ColeccionesG/ICollection.h"
 #include "ColeccionesG/IDictionary.h"
+#include "ColeccionesG/ListaDicc.h"
+#include "ColeccionesG/Lista.h"
+#include "ColeccionesG/KeyString.h"
 #include "Usuarios.h"
+#include "Asignaturas.h"
 #include "string"
 
 class Estudiantes: public Usuarios{
 private:
     string cedula;
 public:
-    Estudiantes (int, string, string, string, string, ICollection*, ICollection*, string);
+    Estudiantes (int,string, string, string, string, ICollection*, ICollection*, string);
     void setCedula(string);
     string getCedula();
     float asistenciaClaseVivo();
     void finalizaciónAsistenciaVivo();
-    void inscripcionAsignatura();
+    void inscripcionAsignatura(IDictionary*);
 };
 
 #endif /* ESTUDIANTES_H */
