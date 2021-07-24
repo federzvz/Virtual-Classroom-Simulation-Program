7b39eb3f9e50a9a914f3de096bf9c413b5a07e82
DieterPreuss <87830546+DieterPreuss@users.noreply.github.com>
2021-07-24 20:00:22 -0300

    Update Administrador.cpp

--- Administrador.cpp
+++ Administrador.cpp
@@ -39,8 +39,39 @@ void Administrador::altaAsignatura(string nombre, string codigo, bool teorica, b
 };
 
 
-void Administrador::asigDocenteAsignatura(){
+void Administrador::asigDocenteAsignatura(int ID,string codAsig,IDictionary* listaDocente,IDictionary* listaAsig){
+    KeyInt* keyDoc = new KeyInt(ID);
+    KeyString *keyAsig= new KeyString(codAsig);
     
+    Usuarios *Doc_x =  (Usuarios*) listaDocente->find(keyDoc);
+    Asignaturas *Asig_x = (Asignaturas*) listaAsig->find(keyAsig);
+        
+    Doc_x->addListaClases(Asig_x);
+    
+    IIterator *Indice = Doc_x->getListaAsig()->iterator();
+    
+    cout<<"Docente: "<<Doc_x->getNombre()<<endl;
+    while(Indice->hasNext()){
+        Asignaturas* Mostrar_Asig = (Asignaturas*) Indice->getCurrent();
+        cout<<"Nombre: "<<Mostrar_Asig->getNombre()<<endl;
+        cout<<"ID Asig: "<<Mostrar_Asig->getCodigo()<<endl;
+        if(Mostrar_Asig->getTeoricas()){
+            cout<<"Clases Teoricas: Si"<<endl;
+        }
+        else
+            cout<<"Clases Teoricas: No"<<endl;
+        if(Mostrar_Asig->getPracticas()){
+            cout<<"Clases Practicas: Si"<<endl;
+        }
+        else
+            cout<<"Clases Practicas: No"<<endl;
+        if(Mostrar_Asig->getMonitoreo()){
+            cout<<"Clases Monitoreo: Si"<<endl;
+        }
+        else
+            cout<<"Clases Monitoreo: No"<<endl;
+        Indice->next();
+    }
 };
 
 void Administrador::eliminarAsignatura(){
